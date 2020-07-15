9ff3984623d41825382251d3030ad32fedda3fad
karthiksharma1992 <karthik.sharma.1992@gmail.com>
2020-07-15 22:22:14 +0200

    Commit #3 : Data and Methods

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 new Vue({
   el : '#vue-app',
   data : {
+    pageTitle : 'Project 1',
+    pageSubTitle : 'Vuejs tutorial',
     bottomMessage : 'Project by KGS',
+    name : 'Karthik!'
   },
+  methods : {
+    greet:function(timeOfDay){
+      return "Good " + timeOfDay +" " + this.name + "!";
+    }
+  }
 });
