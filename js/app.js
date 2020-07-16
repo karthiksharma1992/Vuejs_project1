647c6329ba323f8ecf2925c8ce113144545a3136
karthiksharma1992 <karthik.sharma.1992@gmail.com>
2020-07-16 15:02:37 +0200

    Commit#4 : Data Binding

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ new Vue({
     pageTitle : 'Project 1',
     pageSubTitle : 'Vuejs tutorial',
     bottomMessage : 'Project by KGS',
-    name : 'Karthik!'
+    name : 'Karthik!',
+    website : 'https://www.google.com',
+    websiteTag : '<p>Click here to visit <a href="https://www.orsted.com">Orsted</a></p>',
   },
   methods : {
     greet:function(timeOfDay){
