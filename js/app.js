066db85dbe3b627cfc208fc3f14a457598e3431f
karthiksharma1992 <karthik.sharma.1992@gmail.com>
2020-07-22 20:26:15 +0200

    Commit#5 : Events

--- js/app.js
+++ js/app.js
@@ -5,12 +5,25 @@ new Vue({
     pageSubTitle : 'Vuejs tutorial',
     bottomMessage : 'Project by KGS',
     name : 'Karthik!',
+    age : 27,
     website : 'https://www.google.com',
     websiteTag : '<p>Click here to visit <a href="https://www.orsted.com">Orsted</a></p>',
+    x: 0,
+    y: 0,
   },
   methods : {
     greet:function(timeOfDay){
       return "Good " + timeOfDay +" " + this.name + "!";
+    },
+    older:function(inc){
+      this.age += inc;
+    },
+    younger:function(dec){
+      this.age -= dec;
+    },
+    updateXY:function(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
